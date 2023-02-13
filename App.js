16bbb9e13a992ac0a0957af6897b2c7a219d68c4
Filipi Fuchter <filipi87@gmail.com>
2023-02-13 17:01:44 -0300

    Joining a call using daily and expo

--- App.js
+++ App.js
@@ -1,28 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect } from "react";
-import { Platform } from "react-native";
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useEffect, useState, useCallback} from 'react';
 import Daily, {DailyMediaView, DailyEventObjectParticipant} from '@daily-co/react-native-daily-js';
-
-export default function App() {
-
-  Platform.OS !== "web" && useEffect(async () => {
-    console.log('Options', await navigator.mediaDevices.enumerateDevices())
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app =) Nice!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+  },
+  media: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    aspectRatio: 9/16,
   },
 });
+
+export default function App() {
+
+  const [videoTrack, setVideoTrack] = useState(null);
+  const [callObject, setCallObject] = useState(null);
+
+  const handleNewParticipantsState = useCallback(
+    (event: DailyEventObjectParticipant) => {
+      // ignoring to don't show the video from the local participant
+      if (event.participant.local) {
+        return;
+      }
+      const videoTrack = event.participant.tracks.video;
+      setVideoTrack(videoTrack.track);
+    },
+    [],
+  );
+
+  useEffect(() => {
+    if (!callObject) {
+      return;
+    }
+    callObject.on('participant-joined', handleNewParticipantsState);
+    callObject.on('participant-updated', handleNewParticipantsState);
+  }, [handleNewParticipantsState, callObject]);
+
+  // Create the callObject and join the meeting
+  useEffect(() => {
+    const call = Daily.createCallObject();
+    call.join({
+      // TODO add your URL here
+      url: 'https://YOUR_DOMAIN.daily.co/YOUR_ROOM',
+    });
+    setCallObject(call);
+    return () => {};
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Sample Daily call with Expo</Text>
+      <DailyMediaView
+        videoTrack={videoTrack}
+        mirror={false}
+        objectFit="cover"
+        style={styles.media}
+      />
+    </SafeAreaView>
+  );
+}
