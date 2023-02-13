6d147816e5bc7471de47146c2c39d673908c7833
Filipi Fuchter <filipi87@gmail.com>
2023-02-13 12:10:22 -0300

    Adding react-native-webrtc

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { Platform } from "react-native";
+import { registerGlobals } from "react-native-webrtc";
 
 export default function App() {
+
+  Platform.OS !== "web" && useEffect(async () => {
+    console.log('Registering webrtc globals')
+    registerGlobals();
+    console.log('Options', await navigator.mediaDevices.enumerateDevices())
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app =) Nice!</Text>
