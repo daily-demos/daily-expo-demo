6d9a386297614a186ca709d5e43105a551b2c461
Filipi Fuchter <filipi87@gmail.com>
2023-02-16 09:04:01 -0300

    - Changing the bundle identifier - Fixing lint - Improving the messages

--- src/App.js
+++ src/App.js
@@ -1,55 +1,65 @@
-import {View, SafeAreaView, StyleSheet, Text, Button, TextInput} from 'react-native';
-import React, {useEffect, useState, useCallback} from 'react';
-import Daily, {DailyMediaView, DailyEventObjectParticipant} from '@daily-co/react-native-daily-js';
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Button,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+import Daily, {
+  DailyMediaView,
+  DailyEventObjectParticipant,
+} from "@daily-co/react-native-daily-js";
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#f7f9fa',
-    width: '100%',
+    backgroundColor: "#f7f9fa",
+    width: "100%",
   },
   outCallContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   inCallContainer: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
+    position: "absolute",
+    width: "100%",
+    height: "100%",
   },
   dailyMediaView: {
     flex: 1,
-    aspectRatio: 9/16,
+    aspectRatio: 9 / 16,
   },
   roomUrlInput: {
     borderRadius: 8,
     marginVertical: 8,
     padding: 12,
-    fontStyle: 'normal',
-    fontWeight: 'normal',
+    fontStyle: "normal",
+    fontWeight: "normal",
     borderWidth: 1,
-    width: '100%',
+    width: "100%",
   },
   infoView: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   controlButton: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
 
-const ROOM_URL_TEMPLATE = 'https://filipi.daily.co/public';
+const ROOM_URL_TEMPLATE = "https://YOUR_DOMAIN.daily.co/YOUR_ROOM";
 
 export default function App() {
-
   const [videoTrack, setVideoTrack] = useState();
   const [callObject, setCallObject] = useState();
   const [inCall, setInCall] = useState(false);
   const [roomUrl, setRoomUrl] = useState(ROOM_URL_TEMPLATE);
-  const [amountOfRemoteParticipants, setAmountOfRemoteParticipants] = useState(0);
+  const [amountOfRemoteParticipants, setAmountOfRemoteParticipants] =
+    useState(0);
 
   const handleNewParticipantsState = (event: DailyEventObjectParticipant) => {
     // ignoring to don't show the video from the local participant
@@ -59,20 +69,20 @@ export default function App() {
     const videoTrack = event.participant.tracks.video;
     setVideoTrack(videoTrack.track);
     //-1 to ignore the local participant
-    setAmountOfRemoteParticipants(callObject.participantCounts().present - 1)
-  }
+    setAmountOfRemoteParticipants(callObject.participantCounts().present - 1);
+  };
 
   const joinRoom = () => {
-    console.log('Invoking to join')
+    console.log("Invoking to join");
     callObject.join({
       url: roomUrl,
     });
-  }
+  };
 
   const leaveRoom = async () => {
-    console.log('Invoking to leave')
+    console.log("Invoking to leave");
     await callObject.leave();
-    let videos = document.getElementsByTagName('video');
+    let videos = document.getElementsByTagName("video");
     for (let vid of videos) {
       vid.remove();
     }
@@ -87,14 +97,14 @@ export default function App() {
 
   //Add the listeners
   useEffect(() => {
-    if(!callObject) {
-      return
+    if (!callObject) {
+      return;
     }
-    callObject.on('joined-meeting', () => setInCall(true));
-    callObject.on('left-meeting', () => setInCall(false));
-    callObject.on('participant-joined', handleNewParticipantsState);
-    callObject.on('participant-updated', handleNewParticipantsState);
-    callObject.on('participant-left', handleNewParticipantsState);
+    callObject.on("joined-meeting", () => setInCall(true));
+    callObject.on("left-meeting", () => setInCall(false));
+    callObject.on("participant-joined", handleNewParticipantsState);
+    callObject.on("participant-updated", handleNewParticipantsState);
+    callObject.on("participant-left", handleNewParticipantsState);
     return () => {};
   }, [callObject]);
 
@@ -102,7 +112,7 @@ export default function App() {
     <SafeAreaView style={styles.safeArea}>
       {inCall ? (
         <View style={styles.inCallContainer}>
-          { amountOfRemoteParticipants > 0 ? (
+          {amountOfRemoteParticipants > 0 ? (
             <DailyMediaView
               videoTrack={videoTrack}
               mirror={false}
@@ -111,19 +121,33 @@ export default function App() {
             />
           ) : (
             <View style={styles.infoView}>
-              <Text>There is no one else in this call yet!</Text>
-              <Text>Invite the participants to join this call:</Text>
+              <Text>No one else is in the call yet!</Text>
+              <Text>Invite others to join the call using this link:</Text>
               <Text>{roomUrl}</Text>
             </View>
           )}
-          <Button style={styles.controlButton} onPress={() => leaveRoom()} title="Leave call"></Button>
+          <Button
+            style={styles.controlButton}
+            onPress={() => leaveRoom()}
+            title="Leave call"
+          ></Button>
         </View>
       ) : (
         <View style={styles.outCallContainer}>
           <View style={styles.infoView}>
             <Text>Not in a call yet</Text>
-            <TextInput style={styles.roomUrlInput} value={roomUrl} onChangeText={(e) => setRoomUrl(e.target.value)} />
-            <Button style={styles.controlButton}  onPress={() => joinRoom()} title="Join call"></Button>
+            <TextInput
+              style={styles.roomUrlInput}
+              value={roomUrl}
+              onChangeText={(newRoomURL) => {
+                setRoomUrl(newRoomURL);
+              }}
+            />
+            <Button
+              style={styles.controlButton}
+              onPress={() => joinRoom()}
+              title="Join call"
+            ></Button>
           </View>
         </View>
       )}
