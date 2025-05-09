833b1e71ebe677157a47ea63e0240099ce3c9425
Filipi Fuchter <filipi87@gmail.com>
2025-05-09 08:47:22 -0300

    Creating script to clear and rebuild the app.

---
+++ scripts/clear_and_rebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# clear
+rm -rf ../ios/
+rm -rf ../android/
+# Use the version of node specified in .nvmrc
+nvm i
+# Install dependencies
+npm i
+# Before a native app can be compiled, the native source code must be generated.
+npx expo prebuild
